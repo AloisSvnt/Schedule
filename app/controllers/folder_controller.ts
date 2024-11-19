092b03a64f71c284772f9743989da55e3a018361
AloisSvnt <alois.sauvonnet@digitalconcept.fr>
2024-11-19 13:31:41 +0100

    fix: remove perso. slugify for Helper/String from adonisCore

--- app/controllers/folder_controller.ts
+++ app/controllers/folder_controller.ts
@@ -1,6 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import Folder from '#models/folder' 
-import slugify from '#utils/slugify'
+import string from '@adonisjs/core/helpers/string'
 
 export default class FolderController {
   /**
@@ -23,7 +23,15 @@ export default class FolderController {
    */
   async store({ request, response, session }: HttpContext) {
     const folderData = request.only(['name', 'totalWorkTime']) as { name: string, totalWorkTime: number, slug?: string }
-    folderData.slug = slugify(folderData.name)
+    let slug = string.slug(folderData.name, { remove: /[*+~.()'"!:@]/g })
+    
+    const existingFolder = await Folder.query().where('slug', 'like', `${slug}%`).orderBy('slug', 'desc').first()
+    if (existingFolder) {
+      const suffix = existingFolder.slug.match(/-(\d+)$/)?.[1]
+      slug = `${slug}-${suffix ? parseInt(suffix) + 1 : 1}`
+    }
+    
+    folderData.slug = slug
     await Folder.create(folderData)
     session.flash({ success: 'Folder created successfully' })
     return response.redirect().toRoute('folders.index')
@@ -49,15 +57,31 @@ export default class FolderController {
    * Handle form submission for the edit action
    */
   async update({ params, request, response, session }: HttpContext) {
-    if(!params || !params.slug) {
+    if (!params || !params.slug) {
       return response.status(400).send('Bad request')
     }
     const folder = await Folder.findBy('slug', params.slug)
-    if(!folder) {
+    if (!folder) {
       return response.status(404).send('Folder not found')
     }
     const folderData = request.only(['name', 'totalWorkTime']) as { name: string, totalWorkTime: number, slug?: string }
-    folderData.slug = slugify(folderData.name)
+    let slug = string.slug(folderData.name, { remove: /[*+~.()'"!:@]/g })
+  
+    // Check if the new slug already exists, excluding the current folder
+    const existingFolder = await Folder.query().where('slug', slug).whereNot('id', folder.id).first()
+    if (existingFolder) {
+      // If the exact slug exists, find the highest suffix and increment it
+      const existingFolderWithSuffix = await Folder.query().where('slug', 'like', `${slug}-%`).whereNot('id', folder.id).orderBy('slug', 'desc').first()
+      if (existingFolderWithSuffix) {
+        const suffixMatch = existingFolderWithSuffix.slug.match(/-(\d+)$/)
+        const suffix = suffixMatch ? parseInt(suffixMatch[1]) + 1 : 1
+        slug = `${slug}-${suffix}`
+      } else {
+        slug = `${slug}-1`
+      }
+    }
+  
+    folderData.slug = slug
     folder.merge(folderData)
     await folder.save()
     session.flash({ success: 'Folder updated successfully' })
