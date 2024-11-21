ed89323589be5b66147c7bdb8b20e2f1ad860878
AloisSvnt <alois.sauvonnet@digitalconcept.fr>
2024-11-21 15:57:31 +0100

    fix: remove comments

--- database/migrations/1731934653523_create_add_slug_to_folders_table.ts
+++ database/migrations/1731934653523_create_add_slug_to_folders_table.ts
@@ -4,10 +4,8 @@ export default class extends BaseSchema {
   protected tableName = 'folders'
 
   async up() {
-    // Vider la table
     await this.db.rawQuery(`DELETE FROM ${this.tableName}`)
 
-    // Ajouter la colonne slug avec les contraintes
     this.schema.alterTable(this.tableName, (table) => {
       table.string('slug').unique().notNullable().defaultTo('')
     })
